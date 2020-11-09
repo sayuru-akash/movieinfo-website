599eaaba6de1840489880c0d8954aec45bb384e6
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2020-11-09 19:14:01 +0530

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,30 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-<body>
-</body>
-</html>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contactData";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+//Insert data
+$sql = "INSERT INTO contactForm (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+if (mysqli_query($conn, $sql)) {
+  $status = "SUCCESS!!";
+} else {
+  $status = "ERROR!: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
